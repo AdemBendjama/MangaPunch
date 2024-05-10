99eb76815de25dbe0f6b50f2a3db81860783ea53
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-05-10 18:37:40 +0100

    Add New Button variant and impliment it in slideshow

---
+++ src/components/ui/button-variants/button-with-icon.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import React from "react";
+
+export function ButtonWithIcon({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex justify-start">
+      <Button className="bg-primary text-primary-foreground text-[14px] leading-none rounded-e-[0]">
+        {children}
+      </Button>
+      <Button
+        size="icon"
+        className="bg-hover rounded-s-[0] w-[2rem] p-[0.5rem]"
+      >
+        <ChevronRight
+          size={16}
+          className="stroke-hover-foreground"
+          absoluteStrokeWidth
+        />
+      </Button>
+    </div>
+  );
+}
--- src/components/ui/slide-show/slide.tsx
+++ src/components/ui/slide-show/slide.tsx
@@ -1,6 +1,7 @@
 import HeartIcon from "@/components/icons/heart-icon";
 import StarIcon from "@/components/icons/star-icon";
 import Image from "next/image";
+import { ButtonWithIcon } from "../button-variants/button-with-icon";
 
 function Slide({ src }: { src: string }) {
   return (
@@ -8,23 +9,26 @@ function Slide({ src }: { src: string }) {
       <div className="w-[70%] py-[1rem] px-[4rem]">
         <div className="py-[1rem] flex gap-[1rem]">
           <div className="bg-orange-500 w-[13.125rem] h-[20.3125rem] rounded-[0.375rem]"></div>
-          <div className="w-[31.25rem] flex flex-col gap-[1rem]">
-            <div className="text-[1.875rem]">Chainsaw Man</div>
-            <div className="text-[1rem]">
-              The name says it all! Denji&apos;s life of poverty is changed
-              forever when he merges with his pet chainsaw dog, Pochita! Now
-              he&apos;s living in the big city and an official Devil Hunter. But
-              he&apos;s got a lot to learn about his new job and chainsaw
-              powers!
+          <div className="w-[31.25rem] flex flex-col justify-between">
+            <div className="flex flex-col gap-[1rem]">
+              <div className="text-[1.875rem]">Chainsaw Man</div>
+              <div className="text-[1rem]">
+                The name says it all! Denji&apos;s life of poverty is changed
+                forever when he merges with his pet chainsaw dog, Pochita! Now
+                he&apos;s living in the big city and an official Devil Hunter.
+                But he&apos;s got a lot to learn about his new job and chainsaw
+                powers!
+              </div>
+              <div className="flex gap-[0.5rem]">
+                <StarIcon />
+                <span>8.7/100</span>
+              </div>
+              <div className="flex gap-[0.5rem]">
+                <HeartIcon />
+                <span>#1 Most Popular All time</span>
+              </div>
             </div>
-            <div className="flex gap-[0.5rem]">
-              <StarIcon />
-              <span>8.7/100</span>
-            </div>
-            <div className="flex gap-[0.5rem]">
-              <HeartIcon />
-              <span>#1 Most Popular All time</span>
-            </div>
+            <ButtonWithIcon>Read More</ButtonWithIcon>
           </div>
         </div>
       </div>
