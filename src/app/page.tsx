017a26df225b73986fda87e6ee4feae934dd0821
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-05-14 10:39:22 +0100

    finish inserting data into sections/categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,14 +37,89 @@ const slidesTrending = [
     title: "Renai Daikou",
   },
 ];
+const slidesShounen = [
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx105778-74YTFlQzcFPg.png",
+    title: "Chainsaw Man",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx53390-1RsuABC34P9D.jpg",
+    title: "Shingeki no Kyojin",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx101517-H3TdM3g5ZUe9.jpg",
+    title: "Jujutsu Kaisen",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx30013-ulXvn0lzWvsz.jpg",
+    title: "ONE PIECE",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx87216-c9bSNVD10UuD.png",
+    title: "Kimetsu no Yaiba",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx85486-INqnYx8gL3eX.jpg",
+    title: "Boku no Hero Academia",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx108556-NHjkz0BNJhLx.jpg",
+    title: "SPY×FAMILY",
+  },
+];
+const slidesSeinen = [
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx30002-7EzO7o21jzeF.jpg",
+    title: "Berserk",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx63327-4KfhQ64AklrK.png",
+    title: "Tokyo Ghoul",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx34632-u4d8Pf7p9fl9.png",
+    title: "Oyasumi Punpun",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx74347-O6KMkECzHPOE.jpg",
+    title: "One Punch-Man",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx30656-dNNIEyEIF66b.png",
+    title: "Vagabond",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx30642-1l7lI2LIt1LV.jpg",
+    title: "Vinland Saga",
+  },
+  {
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx86635-EdaLQmsn86Fy.png",
+    title: "Kaguya-sama: Love is War",
+  },
+];
+
 export default function Home() {
   return (
     <>
       <SlideShow />
       <div className="flex flex-col gap-[2rem] pt-[4rem] pb-[10rem]">
         <SlideCategory title="Trending" slides={slidesTrending} />
-        <SlideCategory title="Shonen" slides={slidesTrending} />
-        <SlideCategory title="Seinen" slides={slidesTrending} />
+        <SlideCategory title="Shonen" slides={slidesShounen} />
+        <SlideCategory title="Seinen" slides={slidesSeinen} />
       </div>
     </>
   );
