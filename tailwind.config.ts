92832b4363f61d465f058f6d5f50360e36b23b17
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-05-15 17:12:57 +0100

    use hsla instead of rgba for slideshow shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,8 +92,8 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       boxShadow: {
-        "slide-inner": "inset 60vw 0 250px 0 rgba(0, 0, 0, 0.9)",
-        "slide-inner-sm": "inset 100vw 0 2px 0 rgba(0, 0, 0, 0.8)",
+        "slide-inner": "inset 60vw 0 250px 0 hsla(0,0%,0%,0.9)",
+        "slide-inner-sm": "inset 100vw 0 2px 0 hsla(0,0%,0%,0.8)",
       },
     },
   },
