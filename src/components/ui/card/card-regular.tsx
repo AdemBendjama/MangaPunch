5fdf172f2b09b643a3137d3f317fd360ce27ffea
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-06-09 21:03:41 +0100

    make ranks display responsive

--- src/components/ui/card/card-regular.tsx
+++ src/components/ui/card/card-regular.tsx
@@ -4,7 +4,7 @@ function CardRegular({ rank }: { rank?: boolean }) {
   return (
     <div className="flex flex-col gap-[0.5rem] 3xl:w-[9vw] xl:w-[13vw] lg:w-[12.5vw] md:w-[16vw] xs:w-[20vw] w-[28vw] relative">
       {rank && (
-        <div className="flex absolute z-10 top-[-10px] left-[-10px] justify-center items-center text-white font-bold pt-[0.125rem] w-[2.5rem] h-[2.5rem] rounded-[50%] bg-primary">
+        <div className="flex absolute z-10 top-[-10px] left-[-10px] justify-center items-center text-white sm:text-base text-sm font-bold sm:pt-[0.125rem] sm:w-[2.5rem] sm:h-[2.5rem] w-[2rem] h-[2rem] rounded-[50%] bg-primary">
           #1
         </div>
       )}
