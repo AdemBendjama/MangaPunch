1f88140bc04406cb09a10eb45fbd5fe814fed7cb
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-06-12 18:33:27 +0100

    adjust top rated page layout

--- src/app/manga/top-rated/page.tsx
+++ src/app/manga/top-rated/page.tsx
@@ -2,11 +2,11 @@ import CardRegular from "@/components/ui/card/card-regular";
 
 function MangaTopRatedPage() {
   return (
-    <div className="xl:px-[11rem] lg:px-[9rem] sm:px-[2rem] px-[1rem] pb-[10rem]">
+    <div className="lg:mx-auto lg:w-[63rem] lg:px-[0rem] sm:px-[2rem] px-[1rem] pb-[10rem]">
       <div className="flex justify-center sm:text-[1.75rem] xs:text-2xl text-lg font-bold sm:py-[2.5rem] py-[2rem]">
         Most Popular Manga of All Time
       </div>
-      <div className="grid 3xl:grid-cols-[repeat(7,_9vw)] lg:grid-cols-[repeat(5,_13vw)] md:grid-cols-[repeat(5,_16vw)] xs:grid-cols-[repeat(4,_20vw)] grid-cols-[repeat(3,_28vw)] gap-y-[0.625rem] justify-between items-stretch">
+      <div className="grid lg:grid-cols-[repeat(5,_176px)] md:grid-cols-[repeat(5,_16vw)] grid-cols-[repeat(3,_28vw)] gap-y-[0.625rem] justify-between items-stretch">
         <CardRegular rank={true} />
         <CardRegular rank={true} />
         <CardRegular rank={true} />
--- src/components/ui/card/card-regular.tsx
+++ src/components/ui/card/card-regular.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 function CardRegular({ rank }: { rank?: boolean }) {
   return (
-    <div className="flex flex-col gap-[0.5rem] 3xl:w-[9vw] xl:w-[13vw] lg:w-[12.5vw] md:w-[16vw] xs:w-[20vw] w-[28vw] relative">
+    <div className="flex flex-col gap-[0.5rem] lg:w-[176px] md:w-[16vw] w-[28vw] relative">
       {rank && (
         <div className="flex absolute z-10 top-[-10px] left-[-10px] justify-center items-center text-white sm:text-base text-sm font-bold sm:pt-[0.125rem] sm:w-[2.5rem] sm:h-[2.5rem] w-[2rem] h-[2rem] rounded-[50%] bg-primary">
           #1
         </div>
       )}
-      <div className="3xl:h-[14.5vw] lg:h-[20vw] md:h-[26vw] xs:h-[31vw] h-[45.5vw] rounded-[6px] overflow-hidden relative">
+      <div className="lg:h-[276px] md:h-[26vw] h-[45.5vw] rounded-[6px] overflow-hidden relative">
         <Image
           src="https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx105778-74YTFlQzcFPg.png"
           alt="cover image"
