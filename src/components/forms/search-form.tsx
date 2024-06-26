08118b137a69f97d5d6503f6d8620edf1fcd4cae
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-06-26 16:26:16 +0100

    change search bar to type 'search' for mobile submission

--- src/components/forms/search-form.tsx
+++ src/components/forms/search-form.tsx
@@ -17,7 +17,7 @@ export function SearchForm({
   const formFields = [
     {
       name: "search",
-      fieldType: "text",
+      fieldType: "search",
       inputClassname: `flex justify-start items-center mt-[0px]
             sm:w-[29rem] w-full h-[3rem] p-[0.5rem] rounded-none border-0 
             bg-background-secondary dark:bg-background text-black dark:text-foreground`,
